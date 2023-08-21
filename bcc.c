24760a90b48233cb5f5d2b87a3f3c8298f4a4748
David Demelier <markand@malikania.fr>
2023-08-21 15:48:08 +0200

    bcc: fix on OpenBSD

--- bcc.c
+++ bcc.c
@@ -22,6 +22,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#if defined(__OpenBSD__)
+#       include <unistd.h>
+#endif
+
 #include "arg.h"
 
 static const char *charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";
